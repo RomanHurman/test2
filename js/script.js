a311d370542c48813cc81f7bfc0d5c6bf75322ae
RomanHurman <44196459+RomanHurman@users.noreply.github.com>
2020-02-13 16:45:13 +0200

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     $('#slick_img').slick({
-        
+        asNavFor: '#work_step',
     });
+    $('#work_step').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '#slick_img',
+        arrows: false
+      });
     new WOW().init();
 })
